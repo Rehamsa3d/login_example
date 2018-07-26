7ff250acc35da16e6abec7a6c3d0cbf13fe90a1d
ahmedxiio <ahmedxiio@raoufs-mbp.zte.com.cn>
2018-07-26 22:45:20 +0200

    try linkedin signin

--- login_example/AppDelegate.swift
+++ login_example/AppDelegate.swift
@@ -8,58 +8,39 @@
 
 import UIKit
 import GoogleSignIn
-
+import LinkedinSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         // Initialize sign-in
         GIDSignIn.sharedInstance().clientID = "354462999218-89oi8ajjmb93909qsqce2jk7rbnu7rvn.apps.googleusercontent.com"
-//        GIDSignIn.sharedInstance().delegate = self
-
+        //        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
     // [START openurl]
     func application(_ application: UIApplication,
                      open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url,
-                                                 sourceApplication: sourceApplication,
-                                                 annotation: annotation)
+        // Linkedin sdk handle redirect
+        if LinkedinSwiftHelper.shouldHandle(url) {
+            return LinkedinSwiftHelper.application(application, open: url, sourceApplication: nil, annotation: nil)
+        }else{
+        return GIDSignIn.sharedInstance().handle(url,sourceApplication: sourceApplication,annotation: annotation)
+        }
     }
     // [END openurl]
     @available(iOS 9.0, *)
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url,
-                                                 sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
-                                                 annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        return GIDSignIn.sharedInstance().handle(url,sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
-//    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
-//              withError error: Error!) {
-//        if let error = error {
-//            print("\(error.localizedDescription)")
-//        } else {
-//            // Perform any operations on signed in user here.
-//            let userId = user.userID                  // For client-side use only!
-//            let idToken = user.authentication.idToken // Safe to send to the server
-//            let fullName = user.profile.name
-//            let givenName = user.profile.givenName
-//            let familyName = user.profile.familyName
-//            let email = user.profile.email
-//            // ...
-//        }
-//    }
-//
-//    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
-//              withError error: Error!) {
-//        // Perform any operations when the user disconnects from app here.
-//        // ...
-//    }
-
+    
+    
 }
 
--- login_example/controller/FirstStep.swift
+++ login_example/controller/FirstStep.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 import GoogleSignIn
-//#import <linkedin-sdk/LISDK.h>
-
+import LinkedinSwift
 class FirstStep: UIViewController {
-
+    
     @IBOutlet weak var signInButton: GIDSignInButton!
+    
     
     //viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         googleSignIn()
+    }
+    
+    @IBAction func linkedinBtnTaped(_ sender: Any) {
+        linkedinSI()
     }
     
     //google SignIn
@@ -34,40 +38,62 @@ class FirstStep: UIViewController {
         
     }
     
+    //linkedin signin func
+    func linkedinSI()  {
+        // You still need to set appId and URLScheme in Info.plist, follow this instruction: https://developer.linkedin.com/docs/ios-sdk
+        let linkedinHelper = LinkedinSwiftHelper(configuration: LinkedinSwiftConfiguration(clientId: "86lumo1mbw18oj", clientSecret: "iqkDGYpWdhf7WKzA", state: "DLKDJF46ikMMZADfdfds", permissions: ["r_basicprofile", "r_emailaddress"], redirectUrl: "https://github.com"))
+        linkedinHelper.authorizeSuccess({ (token) in
+            
+            print(token)
+            //This token is useful for fetching profile info from LinkedIn server
+        }, error: { (error) in
+            
+            print(error.localizedDescription)
+            //show respective error
+        }) {
+            //show sign in cancelled event
+        }
+        
+        //requestURL
+        linkedinHelper.requestURL("https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url,picture-urls::(original),positions,date-of-birth,phone-numbers,location)?format=json", requestType: LinkedinSwiftRequestGet, success: { (response) -> Void in
+            
+            print(response)
+            //parse this response which is in the JSON format
+        }) {(error) -> Void in
+            
+            print(error.localizedDescription)
+            //handle the error
+        }
+        
+    }
 }
 
-//GIDSignInUIDelegate
+//GIDSignInUIDelegate      //google SignIn
+
 extension FirstStep:GIDSignInUIDelegate, GIDSignInDelegate {
     
     //when the signin complets
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         
-                if let error = error {
-                    print("\(error.localizedDescription)")
-                } else {
-                    // Perform any operations on signed in user here.
-                    let userId = user.userID                  // For client-side use only!
-//                    let idToken = user.authentication.idToken // Safe to send to the server
-                    let fullName = user.profile.name
-                    let givenName = user.profile.givenName
-                    let familyName = user.profile.familyName
-                    let email = user.profile.email
-                    
-                    let userINFO = "\(String(describing: userId!))\r\n\(String(describing: fullName!))\r\n\r\n\(String(describing: givenName!))\r\n\(String(describing: familyName!))\r\n\(String(describing: email!))"
-                    
-                    performSegue(withIdentifier: "GoogleVC", sender: userINFO)
-
-                }
-        
-
-    }
-    
-    //
-        func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
-                  withError error: Error!) {
-            // Perform any operations when the user disconnects from app here.
-            // ...
+        if let error = error {
+            print("\(error.localizedDescription)")
+        } else {
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only!
+            //                    let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            
+            let userINFO = "\(String(describing: userId!))\r\n\(String(describing: fullName!))\r\n\r\n\(String(describing: givenName!))\r\n\(String(describing: familyName!))\r\n\(String(describing: email!))"
+            
+            performSegue(withIdentifier: "GoogleVC", sender: userINFO)
+            
         }
+        
+        
+    }
 }
 
 // prepare Segue
